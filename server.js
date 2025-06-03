2b06829d0a6dc041290896a85c411f674dfa8d84
rakhimov-leo <tiktok.odilstudio.uzboom@gmail.com>
2025-06-03 19:09:32 +0900

    feat: develop author portfolio page

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ console.log("Web Serverni boshlash*");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 
 // 1 Kirish code
 app.use(express.static("public"));
@@ -15,16 +25,21 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4 Routing code
-app.get("/hello", function (req, res) {
-  res.end(`<h1 style="background: red"> HELLO WORLD by Leo</h1>`);
+app.post("/create-item", (req, res) => {
+  console.log(req);
+  res.json({ test: "succes" });
 });
 
-app.get("/gift", function (req, res) {
-  res.end(`<h1> siz sovgalar bolimidasiz</h1>`);
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
+});
+
+app.get("/", function (req, res) {
+  res.render("harid");
 });
 
 const server = http.createServer(app);
-let PORT = 3002;
+let PORT = 3000;
 server.listen(PORT, function () {
   console.log(`The server is running successfully on port: ${PORT}`);
 });
