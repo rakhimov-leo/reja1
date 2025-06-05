0c0ec4bd0031aac08f1ea694f0d89242b76cf269
rakhimov-leo <tiktok.odilstudio.uzboom@gmail.com>
2025-06-05 16:42:31 +0900

    feat: train callback hamda asyn functions

--- train.js
+++ train.js
@@ -1,1 +1,163 @@
-console.log("TRAIN AREA!");
+// console.log("TRAIN AREA!");
+
+// 21. NodeJS event loop va Callback functionlarni o'rganamiz
+
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//   "yaxshi talaba bo'ling", // 0-20
+//   "togri boshliq tanlang va koproq hato qiling", // 20-30
+//   "uzingizga ishlashingizni boshlang", // 30-40
+//   "siz kuchli bolgan narsalarni qiling", //40-50
+//   "yoshlarga investitsiya qiling", //50-60
+//   "endi dam oling, foydasi yoq endi", //60
+// ];
+
+//        CALLBACK function
+
+// function maslahatBering(a, callback) {
+//   if (typeof a !== "number") callback("insert a number", null);
+//   else if (a <= 20) callback(null, list[0]);
+//   else if (a > 20 && a <= 30) callback(null, list[1]);
+//   else if (a > 30 && a <= 40) callback(null, list[2]);
+//   else if (a > 40 && a <= 50) callback(null, list[3]);
+//   else if (a > 50 && a <= 60) callback(null, list[4]);
+//   else {
+//     setTimeout(function () {
+//       callback(null, list[5]);
+//     }, 5000);
+//   }
+// }
+
+// maslahatBering(50, (err, data) => {
+//   if (err) console.log("ERROR:", err);
+//   console.log("javob:", data);
+// });
+
+// maslahatBering(32, (err, data) => {
+//   if (err) console.log("ERROR:", err);
+//   console.log("javob:", data);
+// });
+
+// maslahatBering(45, (err, data) => {
+//   if (err) console.log("ERROR:", err);
+//   console.log("javob:", data);
+// });
+
+// maslahatBering(salom, (err, data) => {
+//   if (err) console.log("ERROR:", err);
+//   console.log("javob:", data);
+// });
+
+// console.log("passed here 0");
+// maslahatBering(16, (err, data) => {
+//   if (err) console.log("ERROR:", err);
+//   console.log("javob:", data);
+// });
+// console.log("passed here 1");
+
+//   22. Asynchronous functionlarni qo'llash
+
+// async function maslahatBering(a) {
+//   if (typeof a !== "number") throw new Error("insert a number");
+//   else if (a <= 20) return list[0];
+//   else if (a > 20 && a <= 30) return list[1];
+//   else if (a > 30 && a <= 40) return list[2];
+//   else if (a > 40 && a <= 50) return list[3];
+//   else if (a > 50 && a <= 60) return list[4];
+//   else {
+//     return list[5];
+//    setTimeout(function () {
+//       callback(null, list[5]);
+//     }, 5000);
+//   }
+// }
+
+// CALL via then/catch
+// console.log("passed here 0");
+// maslahatBering(25)
+//   .then((data) => {
+//     console.log("javob:", data);
+//   })
+//   .catch((err) => {
+//     console.log("ERROR:", err);
+//   });
+// console.log("passed here 1");
+
+// async function maslahatBering(a) {
+//   if (typeof a !== "number") throw new Error("insert a number");
+//   else if (a <= 20) return list[0];
+//   else if (a > 20 && a <= 30) return list[1];
+//   else if (a > 30 && a <= 40) return list[2];
+//   else if (a > 40 && a <= 50) return list[3];
+//   else if (a > 50 && a <= 60) return list[4];
+//   else {
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         resolve(list[5]);
+//       }, 5000);
+//     });
+//   }
+// }
+
+// CALL via ASYN/AWAIT
+
+// async function run() {
+//   let javob = await maslahatBering(25);
+//   console.log(javob);
+//   javob = await maslahatBering(71);
+//   console.log(javob);
+//   javob = await maslahatBering(41);
+//   console.log(javob);
+//   javob = await maslahatBering(80);
+//   console.log(javob);
+// }
+// run();
+
+//          callback bn taxlash
+
+// function maslahatBering(a, callback) {
+//   if (typeof a !== "number") callback("insert a number", null);
+//   else if (a <= 20) callback(null, list[0]);
+//   else if (a > 20 && a <= 30) callback(null, list[1]);
+//   else if (a > 30 && a <= 40) callback(null, list[2]);
+//   else if (a > 40 && a <= 50) callback(null, list[3]);
+//   else if (a > 50 && a <= 60) callback(null, list[4]);
+//   else {
+//     setInterval(function () {
+//       callback(null, list[5]);
+//     }, 2000);
+//   }
+// }
+
+// console.log("passed here 0");
+// maslahatBering(70, (err, data) => {
+//   if (err) console.log("ERROR:", err);
+//   console.log("javob:", data);
+// });
+// console.log("passed here 1");
+
+//                        A-TASK:
+
+// Shunday 2 parametrli function tuzing,
+// hamda birinchi parametrdagi
+// letterni ikkinchi parametrdagi sozdan
+//  qatnashga sonini return qilishi kerak boladi.
+// MASALAN countLetter("e", "engineer") 3ni return qiladi.
+
+//             masalani yechimi:
+
+console.log("TASK A.ni birgalikda yechamiz");
+
+function countLetter(letter, word) {
+  let count = 0;
+  for (let char of word) {
+    if (char === letter) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(
+  countLetter("a", "Elektromexanizatsiyalashtraololmaganligingizdandurda")
+);
