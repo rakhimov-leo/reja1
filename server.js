0c13a1200487aea9e06f8f107815e307beaab822
rakhimov-leo <tiktok.odilstudio.uzboom@gmail.com>
2025-06-17 01:19:50 +0900

    feat: pm2 taxladim

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongodb.connect(
 
       const app = require("./app.js");
       const server = http.createServer(app);
-      let PORT = 4800;
+      let PORT = 3008;
       server.listen(PORT, function () {
         console.log(
           `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
